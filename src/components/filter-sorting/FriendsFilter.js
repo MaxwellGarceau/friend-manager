a675bc633b64de95b7a22e0e124d1b6962c32537
Max Garceau <maxwellgarceau@gmail.com>
2018-08-29 10:32:09 -0500

    FInished filter path for relationshipFilter

--- src/components/filter-sorting/FriendsFilter.js
+++ src/components/filter-sorting/FriendsFilter.js
@@ -8,43 +8,97 @@ class FriendsFilter extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
+      selectedFilters: [],
       rankingSliderValue: {
         min: 1,
         max: 5
       }
     };
+  };
+  generateInputField = (type, name, onChange, parent) => {
+    return (
+      <input
+        type={type}
+        name={name}
+        onChange={onChange}
+        data-parent={parent} />
+    );
   }
   handleRankingSliderChange = (rankingSliderValue) => this.setState({ rankingSliderValue });
   handleUpdateFilter = (e) => {
     e.preventDefault();
     // Code that prepares data by setting category to object key and filter values as sub key/values
-
-    // Dummy data for filter test
-    const selectedFilters = [{
-      active: true,
-      filterId: 'relationshipFilter',
-      params: ['friend', 'family']
-    }, {
-      active: false,
-      filterId: 'rankingFilter'
-    }];
+    const selectedFilters = this.state.selectedFilters;
     this.props.startUpdateFriendListFilters(selectedFilters);
+    // Dummy data for filter test
+    // const selectedFilters = [{
+    //   active: true,
+    //   filterId: 'relationshipFilter',
+    //   params: ['friend', 'family']
+    // }, {
+    //   active: false,
+    //   filterId: 'rankingFilter'
+    // }];
+    // this.props.startUpdateFriendListFilters(selectedFilters);
+  };
+  handleInputFieldChange = (e) => {
+    let value;
+    const input = e.target;
+    const parentName = input.dataset.parent;
+    const name = input.name;
+
+    switch (input.type) {
+      case 'checkbox':
+        value = input.checked;
+        break;
+      default:
+        value = input.value;
+    }
+
+    if (!value) {
+      this.setState({
+        selectedFilters: this.state.selectedFilters.filter((filter) => filter.filterId !== parentName)
+      });
+      return;
+    }
+
+    this.setState((prevState) => {
+      const prevFilterState = prevState[parentName];
+      // If prevFilterState AND prevFilterState.params exists then prevParams is prevFilterState.params. Else prevParams is [].
+      // Written this way to avoid undefined error from trying to check an object property that doesn't exist.
+      const prevParams = (prevFilterState) ? prevFilterState.params ? prevFilterState.params : [] : [];
+      return this.state.selectedFilters.push({
+        active: true,
+        filterId: parentName,
+        params: [...prevParams, name]
+      });
+    });
   };
   render () {
     return (
       <div>
         <h2>Filter</h2>
         <form onSubmit={this.handleUpdateFilter}>
-          <fieldset>
+          <fieldset name="relationshipFilter">
             <legend>Relationship</legend>
-            <input type="checkbox" name="friend" value="friend" /> Friend
+            <input
+              type="checkbox"
+              name="friend"
+              onChange={this.handleInputFieldChange}
+              data-parent="relationshipFilter" /> Friend
             <br />
-            <input type="checkbox" name="family" value="family" /> Family
+            <input
+              type="checkbox"
+              name="family"
+              value="family" /> Family
             <br />
-            <input type="checkbox" name="acquaintance" value="acquaintance" /> Acquaintance
+            <input
+              type="checkbox"
+              name="acquaintance"
+              value="acquaintance" /> Acquaintance
             <br />
           </fieldset>
-          <fieldset>
+          <fieldset name="rankingFilter">
             <legend>Ranking</legend>
             <InputRange
               maxValue={5}
@@ -55,7 +109,7 @@ class FriendsFilter extends React.Component {
             />
             <br />
           </fieldset>
-          <fieldset>
+          <fieldset name="locationFilter">
             <legend>Location</legend>
             <br />
           </fieldset>
