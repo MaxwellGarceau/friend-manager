4fb21881ab0029dfbea7ad7903e995ae2aaa54b3
Max Garceau <maxwellgarceau@gmail.com>
2018-09-10 11:01:09 -0400

    Set up onSubmit for ManuallyAddFriend.js

--- src/components/friend-list/ManuallyAddFriend.js
+++ src/components/friend-list/ManuallyAddFriend.js
@@ -1,5 +1,8 @@
 import React from 'react';
-// import _ from 'lodash';
+
+import { connect } from 'react-redux';
+import { startAddFriend } from '../../actions/friends';
+
 import { startCase } from 'lodash-es';
 import StarRatingComponent from 'react-star-rating-component';
 
@@ -8,12 +11,12 @@ import LocationPicker from '../filter-sorting/LocationPicker';
 class ManuallyAddFriend extends React.Component {
   state = {
     relationshipOptions: ['friend', 'family', 'acquaintance'],
-    rank: 5,
+    ranking: 5,
     name: '',
     relationship: 'friend'
   };
   onStarClick = (nextValue, prevValue, name) => {
-    this.setState({ rank: nextValue });
+    this.setState({ ranking: nextValue });
   };
   handleName = (e) => {
     const name = e.target.value;
@@ -24,32 +27,51 @@ class ManuallyAddFriend extends React.Component {
     this.setState({ relationship });
   };
   setLocationState = (location) => this.setState({ location });
+  handleSubmit = () => {
+    const { name, relationship, location, ranking } = this.state;
+    const newFriend = {
+      name,
+      relationship,
+      location,
+      ranking
+    };
+
+    this.props.startAddFriend(newFriend);
+  };
   render () {
     return (
-      <tr className="manually-add-friend">
-        <td align="center"><input type="text" className="text-input" placeholder="Name" value={this.state.name} onChange={this.handleName} /></td>
-        <td align="center">
-          <select value={this.state.relationship} className="select" onChange={this.handleRelationship} placeholder="Relationship">
-            {this.state.relationshipOptions.map((option, ind) => {
-              return <option key={`add-friend-relationship-option-key-${ind}`}>{startCase(option)}</option>;
-            })}
-          </select>
-        </td>
-        <td align="center">
-          <LocationPicker setLocationState={this.setLocationState} />
-        </td>
-        <td align="center">
-          <StarRatingComponent
-            name="manually-add-friend-ranking"
-            value={this.state.rank}
-            onStarClick={this.onStarClick}
-            starCount={5}
-            renderStarIcon={() => <i className="far fa-star"></i>}
-            className="manually-add-friend__dv-star-rating"
-          /></td>
-      </tr>
+      <React.Fragment>
+        <tr className="manually-add-friend">
+          <td align="center"><input type="text" className="text-input" placeholder="Name" value={this.state.name} onChange={this.handleName} /></td>
+          <td align="center">
+            <select value={this.state.relationship} className="select" onChange={this.handleRelationship} placeholder="Relationship">
+              {this.state.relationshipOptions.map((option, ind) => {
+                return <option key={`add-friend-relationship-option-key-${ind}`}>{startCase(option)}</option>;
+              })}
+            </select>
+          </td>
+          <td align="center">
+            <LocationPicker setLocationState={this.setLocationState} />
+          </td>
+          <td align="center">
+            <StarRatingComponent
+              name="manually-add-friend-ranking"
+              value={this.state.rank}
+              onStarClick={this.onStarClick}
+              starCount={5}
+              renderStarIcon={() => <i className="far fa-star"></i>}
+              className="manually-add-friend__dv-star-rating"
+            />
+          </td>
+        </tr>
+        <tr><button type="button" onClick={this.handleSubmit}>Add Friend</button></tr>
+      </React.Fragment>
     );
   }
 }
 
-export default ManuallyAddFriend;
+const mapDispatchToProps = (dispatch) => ({
+  startAddFriend: (newFriend) => dispatch(startAddFriend(newFriend))
+});
+
+export default connect(undefined, mapDispatchToProps)(ManuallyAddFriend);
