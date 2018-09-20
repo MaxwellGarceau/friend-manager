ed0e8bb7ee435c4413eba52876d17184c356a0c3
Max Garceau <maxwellgarceau@gmail.com>
2018-09-20 11:21:59 -0400

    Added modal that pops up asking user to confirm friend deletion before deleting friend

--- src/components/friend-list/ModifyFriendIcons.js
+++ src/components/friend-list/ModifyFriendIcons.js
@@ -4,7 +4,12 @@ import { cloneDeep } from 'lodash-es';
 import { connect } from 'react-redux';
 import { startCanEditFriend, startCancelEditFriend, startDeleteFriend } from '../../actions/friends';
 
+import AlertModal from '../misc-components/AlertModal';
+
 class ModifyFriendIcons extends React.Component {
+  state = {
+    showDeleteModal: false
+  };
   handleCancelEditFriend = (e) => {
     const _id = e.target.dataset.friendId;
     this.props.startCancelEditFriend(_id);
@@ -17,16 +22,18 @@ class ModifyFriendIcons extends React.Component {
       this.props.startCanEditFriend(_id);
     }
   };
-  handleDeleteFriend = (e) => {
-    // CREATE MODAL ASKING USER TO CONFIRM DELETE FIRST
-    const _id = e.target.dataset.friendId;
+  toggleModal = () => {
+    !this.state.showDeleteModal ? this.setState({ showDeleteModal: true }) : this.setState({ showDeleteModal: false });
+  };
+  handleDeleteFriend = () => {
+    const _id = this.props.friend._id;
     this.props.startDeleteFriend(_id);
   };
   render () {
     const { friend } = this.props;
     return (
       <React.Fragment>
-        <div className={`friends-list__delete`} data-friend-id={friend._id} onClick={this.handleDeleteFriend}>
+        <div className={`friends-list__delete`} data-friend-id={friend._id} onClick={this.toggleModal}>
           <i data-friend-id={friend._id} className="fas fa-trash-alt"></i>
         </div>
         <div className={`friends-list__edit`} data-friend-id={friend._id} onClick={this.handleEditFriend}>
@@ -35,6 +42,11 @@ class ModifyFriendIcons extends React.Component {
         {!!friend.canEditFriend && <div className={`friends-list__cancel-edit`} data-friend-id={friend._id} onClick={this.handleCancelEditFriend}>
           <i data-friend-id={friend._id} className="fas fa-times-circle"></i>
         </div>}
+        {!!this.state.showDeleteModal && <AlertModal
+          confirmAction={this.handleDeleteFriend}
+          denyAction={this.toggleModal}
+          mainText="Are you sure you want to delete this contact?"
+          subText="This action can not be undone" />}
       </React.Fragment>
     );
   }
---
+++ src/components/misc-components/AlertModal.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class AlertModal extends React.Component {
+  componentDidMount () {
+    document.querySelector('body').style.overflow = 'hidden';
+  };
+  componentWillUnmount () {
+    document.querySelector('body').style.overflow = 'scroll';
+  };
+  render () {
+    return (
+      <div className="box-layout box-layout--full-page box-layout--mandatory-overlay">
+        <div className="box-layout__box">
+          <h2>{this.props.mainText}</h2>
+          <p className="alert-modal__sub-text">{this.props.subText}</p>
+          <div>
+            <button onClick={this.props.confirmAction}>Confirm</button>
+            <button onClick={this.props.denyAction}>Cancel</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default AlertModal;
