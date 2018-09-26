e6d286519fcad993c0aa80d1944f0b0beedc43c0
Max Garceau <maxwellgarceau@gmail.com>
2018-09-26 11:25:31 -0400

    Added handleStartLogout method to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
+import { history } from '../routers/AppRouterContainer';
 
-export const Header = ({ startLogout }) => (
-  <header className="header">
-    <div className="content-container">
-      <div className="header__content">
-        <Link className="header__title" to="/dashboard">
-          <h1>Friend Manager</h1>
-        </Link>
-        <button className="button button--link" onClick={startLogout}>Logout</button>
-      </div>
-    </div>
-  </header>
-);
+export class Header extends React.Component {
+  handleStartLogout = () => {
+    this.props.startLogout();
+    history.push('/');
+  };
+  render () {
+    return (
+      <header className="header">
+        <div className="content-container">
+          <div className="header__content">
+            <Link className="header__title" to="/dashboard">
+              <h1>Friend Manager</h1>
+            </Link>
+            <button className="button button--link" onClick={this.handleStartLogout}>Logout</button>
+          </div>
+        </div>
+      </header>
+    );
+  }
+};
 
 const mapDispatchToProps = (dispatch) => ({
   startLogout: () => dispatch(startLogout())
