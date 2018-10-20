6e0f8814f7cd095f38cb545f1560b98733711fc1
Max Garceau <maxwellgarceau@gmail.com>
2018-10-20 11:18:50 -0500

    Heroku server troubleshooting 10-20-18 1

--- server/server.js
+++ server/server.js
@@ -178,7 +178,9 @@ app.delete('/api/users/me/token', authenticate, async (req, res) => {
 
 // app.use('../public/dist', express.static(path.join(publicPath, '/dist/bundle.js')));
 
-app.get('*', (req, res) => {
+app.use(express.static(path.join(publicPath, 'dist')));
+
+app.get('/', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
